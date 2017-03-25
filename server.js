e0555c67e42fc82c81d8da001de0089cd1ce4d8b
Stephane Gariepy <sgariepy@MacBook-Pro-de-Stephane.local>
2017-03-25 14:10:07 -0400

    Service siteChecker pour appeler les autres services reliés à des ressources (tel que wot).

--- server.js
+++ server.js
@@ -2,29 +2,31 @@ require("dotenv").config();
 const express       = require('express');
 const app           = express();
 const bodyParser    = require("body-parser");
-const morgan        = require("morgan");
 const PORT          = process.env.PORT || 3000;
-const ENV           = process.env.ENV || "development";
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
 app.get('/', function(req, res){
-  res.render('index');
+    res.render('index');
 });
 
 app.post('/analyse', function(req, res) {
-  // L'information necessaire (le URL est dans req.body.url -- le reste est de passe
-  // ce URL a l'analyse)
+    // L'information necessaire (le URL est dans req.body.url -- le reste est de passe
+    // ce URL a l'analyse)
+    // Ppour passer les resultats de l'analyse, on les passes dans un obnjet quand on render
 
-  // Ppour passer les resultats de l'analyse, on les passes dans un obnjet quand on render
+    const siteCheckerService = require('./services/siteChecker');
 
-  res.render('analysis', {url: req.body.url})
+    siteCheckerService.getResult(req.body.url).then(result => {
+        res.render('analysis', {
+            result: JSON.stringify(result)
+        });
+    });
 
-})
+});
 
 app.listen(PORT, () => {
-  console.log("Against Fake News listening on port " + PORT);
-  console.log(`http://localhost:${PORT}`);
+    console.log("Against Fake News listening on port " + PORT);
+    console.log(`http://localhost:${PORT}`);
 });
--- testwot.js
+++ testwot.js
@@ -1,7 +1,16 @@
-const wotService = require('./services/wot');
+//const wotService = require('./services/wot');
 
 const url = 'http://www.legorafi.fr/2017/03/24/un-membre-du-cabinet-noir-de-francois-hollande-en-arrive-a-douter-de-sa-propre-existence/';
 
-wotService(url).then(result => {
+// wotService(url).then(result => {
+//     console.log(result);
+// });
+
+const siteCheckerService = require('./services/siteChecker');
+
+siteCheckerService.getResult(url).then(result => {
     console.log(result);
 });
+
+
+// require('./services/wot').extractDomain(url);
