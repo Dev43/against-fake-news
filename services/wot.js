889f2ad3e1feeff4de010eabe94853fd81fb0d4c
Stephane Gariepy <sgariepy@MacBook-Pro-de-Stephane.local>
2017-03-25 10:53:07 -0400

    Une réponse de WOT est obtenue.

--- services/wot.js
+++ services/wot.js
@@ -1,12 +1,26 @@
 const Promise = require('bluebird');
-const wotApiBaseUrl = 'http://api.mywot.com/0.4/public_link_json2';
+const request = require('request');
 const wotConfig = require('../config/wotConfig');
+
+
+const wotApiBaseUrl = 'http://api.mywot.com/0.4/public_link_json2';
 
 module.exports = function(url) {
     return new Promise((resolve, reject) => {
-        const requestUrl = `${wotApiBaseUrl}?/hosts=${url}/&key=${wotConfig.apiKey}`;
+        const requestUrl = `${wotApiBaseUrl}/?hosts=${url}/&key=${wotConfig.apiKey}`;
 
-        resolve(requestUrl);
+        var options = {
+            url: requestUrl,
+            method: 'GET',
+            json: true
+        }
+        request(options, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(body);
+            }
+        });
 
     });
 };
