2687ffc1808d5c230ca558d672d894c14d0fe5cd
Dev43 <patrick.guay43@gmail.com>
2017-03-25 14:52:09 -0400

    clean-up of file structure and code

--- application.py
+++ application.py
@@ -6,19 +6,6 @@ from modules.newsScrapper import NewsScrapper
 #Read data from stdin
 print("hello")
 
-# def main():
-#     #get our data as an array from read_in()
-#     lines = read_in()
-
-#     #create a numpy array
-#     np_lines = np.array(lines)
-
-#     #use numpys sum method to find sum of all elements in the array
-#     lines_sum = np.sum(np_lines)
-
-#     #return the sum to the output stream
-#     print lines_sum
-
 def read_in():
     lines = sys.stdin.readlines()
     #Since our input would only be having one line, parse our JSON data from that
--- sentiment.js
+++ sentiment.js
@@ -1,28 +1,14 @@
+const request = require('request');
 require("dotenv").config();
 var spawn = require('child_process').spawn,
-    py    = spawn('python3', ['application.py']),
+    py    = spawn('python3', ['application.py']), // script is partially in application.py
     data = "www.yahoo.com",
     dataString = '';
-
-// const request = require('request');
-// const scraperjs = require('scraperjs');
-
-
 
 // const test = "J'adore la belle journee ";
 // const test1 = "Je deteste le plat Nato";
 // const sentences = [test, test1]
 
-
-
-//   py.stdout.on('data', function(data){
-//   dataString += data.toString();
-// });
-// py.stdout.on('end', function(){
-//   console.log('Sum of numbers=',dataString);
-// });
-// py.stdin.write(JSON.stringify(data));
-// py.stdin.end();
 
 function parseWebsite(url){
 // Scraping a specific website
@@ -35,52 +21,54 @@ function parseWebsite(url){
   py.stdout.on('end', function(){
     console.log('Data received ', dataString);
   });
+
   // Sending information to python script
   py.stdin.write(JSON.stringify(url)); // url to scrape
 
   py.stdin.end();
-
 }
 
+
+function averageSentiment(analysisResponse){
+  return analysisResponse.reduce((acc, next) => {
+    return acc + next.score
+  }, 0) / analysisResponse.length
+}
+
+function aggregateSentences(sentences){
+  const objArray =  [];
+  sentences.forEach((sentence, index) => {
+    objArray.push({
+      "id": index +1,
+      "text": sentence
+    })
+  })
+  console.log(objArray)
+  return (objArray);
+}
+
+const options = {
+  method: 'POST',
+  url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
+  headers: {
+     'cache-control': 'no-cache',
+     "accept": 'application/json',
+     'content-type': 'application/json',
+     'ocp-apim-subscription-key': '0faf54fce59e41719f46aaa3d6687995' },
+  body:{
+    documents: aggregateSentences(sentences)
+  },
+  json: true
+};
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  // get all the scores for all the sentenvces, make a mean score
+  console.log(averageSentiment(body.documents))
+
+});
+
+
+
 parseWebsite("http://yahoo.com")
-
-// function averageSentiment(analysisResponse){
-//   return analysisResponse.reduce((acc, next) => {
-//     return acc + next.score
-//   }, 0) / analysisResponse.length
-// }
-
-// function aggregateSentences(sentences){
-//   const objArray =  [];
-//   sentences.forEach((sentence, index) => {
-//     objArray.push({
-//       "id": index +1,
-//       "text": sentence
-//     })
-//   })
-//   console.log(objArray)
-//   return (objArray);
-// }
-
-// const options = {
-//   method: 'POST',
-//   url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment',
-//   headers: {
-//      'cache-control': 'no-cache',
-//      "accept": 'application/json',
-//      'content-type': 'application/json',
-//      'ocp-apim-subscription-key': '0faf54fce59e41719f46aaa3d6687995' },
-//   body:{
-//     documents: aggregateSentences(sentences)
-//   },
-//   json: true
-// };
-
-// request(options, function (error, response, body) {
-//   if (error) throw new Error(error);
-
-//   // get all the scores for all the sentenvces, make a mean score
-//   console.log(averageSentiment(body.documents))
-
-// });
-
