12b4471509770c394f42a06b0b9dd14aa6ab99c7
Stephane Gariepy <sgariepy@MacBook-Pro-de-Stephane.local>
2017-03-25 10:43:27 -0400

    Formation de l'URL d'appel à l'API WOT.

--- services/wot.js
+++ services/wot.js
@@ -1,6 +1,12 @@
+const Promise = require('bluebird');
+const wotApiBaseUrl = 'http://api.mywot.com/0.4/public_link_json2';
+const wotConfig = require('../config/wotConfig');
 
 module.exports = function(url) {
     return new Promise((resolve, reject) => {
-        resolve(url);
+        const requestUrl = `${wotApiBaseUrl}?/hosts=${url}/&key=${wotConfig.apiKey}`;
+
+        resolve(requestUrl);
+
     });
 };
