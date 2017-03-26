8eae99e1dc8a37dcd3ccc3fa36c61d0b85894ed6
Stephane Gariepy <sgariepy@MacBook-Pro-de-Stephane.local>
2017-03-26 09:53:00 -0400

    Intégration de sentiment module.

--- services/siteChecker.js
+++ services/siteChecker.js
@@ -1,15 +1,18 @@
 const wotService = require('./wot');
 const dateCheck = require('./dateCheck');
+const sentiment = require("../sentiment.js")
 
 module.exports = {
     getResult: getResult
 };
 
 function getResult(url) {
+
     return new Promise((resolve, reject) => {
         Promise.all([
             wotService.getResult(url),
-            dateCheck.check(url)
+            dateCheck.check(url),
+            sentiment.getSentimentPromise(url)
         ])
         .then(results => {
             resolve(calculate(results));
@@ -21,6 +24,7 @@ function getResult(url) {
 function calculate(results) {
     let wotResult = results[0];
     let dateCheck = results[1];
+    let sentimentResult = results[2];
 
     let result = 0;
     if (wotResult.confidence > 0) {
