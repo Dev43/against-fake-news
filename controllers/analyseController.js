e29361b1a1bf5762983f4853e0d55a5c36d2f67c
witold <witoldwrob@gmail.com>
2017-03-26 12:18:36 -0400

    totalScore random

--- controllers/analyseController.js
+++ controllers/analyseController.js
@@ -12,18 +12,22 @@ function post(req, res) {
     const url = req.body.url;
 
     let isSatirical = satirical.isSatirical(url);
+    let totalScore = Math.random()*100
 
     Promise.all([
         sentiment.getSentimentPromise(req.body.url),
-        // relatedArticles.getRelated(req.body.url),
+        relatedArticles.getRelated(req.body.url),
         siteChecker.getResult(url)
     ]).then(results => {
         console.log(results)
+	console.log("HEEEERE    /n")
+       	console.log(results[1][0])
         return res.render('analysis', {
             sentiment: JSON.stringify(results[0]),
             articles: JSON.stringify(results[1]),
             result: JSON.stringify(results[2]),
-            isSatirical: JSON.stringify(isSatirical)
+            isSatirical: JSON.stringify(isSatirical),
+	    totalScore: totalScore
         });
     }).catch((err) => {
         console.log("Error, the error is", err)
